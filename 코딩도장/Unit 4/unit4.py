7c36c736601132438880fe65dbd8b3320759bc30
SYJ0321 <77631167+SYJ0321@users.noreply.github.com>
2022-01-02 17:53:03 +0900

    Create unit4.py

---
+++ 코딩도장/Unit 4/unit4.py
@@ -0,0 +1,18 @@
+print('Hello'); print('1234')
+Hello
+1234
+
+# Hello, world! 출력
+print('Hello, world!')
+Hello, world!
+
+a = 1 + 2 # 더하기
+a
+3
+
+# 더하기
+# a = 1 + 2
+# print ('Hello, world!')
+
+if a==10:
+    print('10입니다.')
