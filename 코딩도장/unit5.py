07732d229e4c8295f822bab811ae80f46bd26519
SYJ0321 <77631167+SYJ0321@users.noreply.github.com>
2022-01-03 23:21:26 +0900

    Add files via upload

---
+++ 코딩도장/unit5.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+1+1
+2
+1-2
+-1
+2*2
+4
+5/2
+2.5
+4/2
+2.0
+5//2
+2
+4//2
+2
+5.5//2
+2.0
+5%2
+1
+2**10
+1024
+int(3.3)
+3
+int(5/2)
+2
+int('10')
+10
+type(10)
+<class 'int'>
+divmod(5,2)
+(2, 1)
+quotient, remainder = divmod(5,2)
+print(quotient, remainder)
+2 1
+0b110
+6
+0o10
+8
+0xF
+15
+3.5+2.1
+5.6
+4.3-2.7
+1.5999999999999996
+1.5*3.1
+4.65
+5.5/3.1
+1.7741935483870968
+4.2+5
+9.2
+float(5)
+5.0
+float(1+2)
+3.0
+float('5.3')
+5.3
+type(3.5)
+<class 'float'>
+1.2+1.3j
+(1.2+1.3j)
+complex(1.2,1.3)
+(1.2+1.3j)
+35+1*2
+37
+(35+1)*2
+72
+print(0.247*12+4.159)
+7.122999999999999
+print(int(0.247*12+4.159))
+7
