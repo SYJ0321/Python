f9c47629ce09eb47f89222fefb83cce2f7b31827
SYJ0321 <77631167+SYJ0321@users.noreply.github.com>
2022-01-03 23:42:57 +0900

    Update unit4.py

--- 코딩도장/Unit 4/unit4.py
+++ 코딩도장/Unit 4/unit4.py
@@ -1,14 +1,14 @@
 print('Hello'); print('1234')
-Hello
-1234
+# Hello
+# 1234
 
 # Hello, world! 출력
 print('Hello, world!')
-Hello, world!
+# Hello, world!
 
 a = 1 + 2 # 더하기
 a
-3
+# 3
 
 # 더하기
 # a = 1 + 2
