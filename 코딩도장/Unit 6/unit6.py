33aa9147536c807826a6b6c4d4c352c91ae99278
SYJ <77631167+SYJ0321@users.noreply.github.com>
2022-01-04 21:38:18 +0900

    Update unit6.py

--- 코딩도장/Unit 6/unit6.py
+++ 코딩도장/Unit 6/unit6.py
@@ -1,131 +1,136 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 변수 할당
 x=10
 x
-10
+# 10
 y='Hello, world!'
 y
-'Hello, world!'
+# 'Hello, world!'
+
+# 변수 자료형
 type(x)
-<class 'int'>
+# <class 'int'>
 type(y)
-<class 'str'>
+# <class 'str'>
+
+# 변수 여러 개 만들기
 x,y,z=10,20,30
 x;y;z
-10
-20
-30
+# 10
+# 20
+# 30
+
 x=y=z=10
 x;y;z
-10
-10
-10
+# 10
+# 10
+# 10
+
 x,y=10,20
 x,y=y,x
 x;y
-20
-10
+# 20
+# 10
+
+# 변수 제거
 x=10
 del x
 x
-Traceback (most recent call last):
-  File "<pyshell#15>", line 1, in <module>
-    x
-NameError: name 'x' is not defined
+# Traceback (most recent call last):
+#   File "<pyshell#15>", line 1, in <module>
+#     x
+# NameError: name 'x' is not defined
+
+# 빈 변수 만들기
 x=None
 print(x)
-None
+# None
 x
 # 아무것도 출력되지 않음
 
+# 변수의 연산
 a=10; b=20
 c = a+b
 c
-30
+# 30
+
 a=10
 a=a+20
 a
-30
+# 30
 a += 20
 a
-50
+# 50
+
 x=-10
 +x
--10
+# -10
 -x
-10
+# 10
 
+# 입력값을 변수에 저장하기
 input()
-Hello, world!
-'Hello, world!'
+# Hello, world!
+# 'Hello, world!'
+
 x = input()
-Hello, world!
+# Hello, world!
 x
-'Hello, world!'
+# 'Hello, world!'
+
 x = input('문자열을 입력하세요: ')
-문자열을 입력하세요: Hello, world!
+# 문자열을 입력하세요: Hello, world!
 x
-'Hello, world!'
+# 'Hello, world!'
 
 a = input('첫번째 숫자를 입력하세요: ')
-첫번째 숫자를 입력하세요: 10
 b = input('두번째 숫자를 입력하세요: ')
-두번째 숫자를 입력하세요: 20
 print(a+b)
-1020
+# 첫번째 숫자를 입력하세요: 10
+# 두번째 숫자를 입력하세요: 20
+# 1020
+
 type(a)
-<class 'str'>
+# <class 'str'>
+
 a=int(input('첫번째 숫자를 입력하세요: '))
-첫번째 숫자를 입력하세요: 10
 b=int(input('두번째 숫자를 입력하세요: '))
-두번째 숫자를 입력하세요: 20
 print(a+b)
-30
+# 첫번째 숫자를 입력하세요: 10
+# 두번째 숫자를 입력하세요: 20
+# 30
+
 a,b=input('문자열 두 개를 입력하세요: ').split()
-문자열 두 개를 입력하세요: Hello Python
+# 문자열 두 개를 입력하세요: Hello Python
 print(a); print(b)
-Hello
-Python
-a,b=input('숫자 두 개를 입력하세요: ').split()
-숫자 두 개를 입력하세요: 10 20
-a=int(a); b=int(b)
-print(a+b)
-30
-map(int, input('숫자 두 개를 입력하세요: ').split())
-숫자 두 개를 입력하세요: 10 20
-<map object at 0x000001E69E37B5B0>
-print(a+b)
-30
+# Hello
+# Python
+
+# input의 결과를 정수형으로 바꾸기
 a,b=map(int, input('숫자 두 개를 입력하세요: ').split())
-숫자 두 개를 입력하세요: 10 20
+# 숫자 두 개를 입력하세요: 10 20
 print(a+b)
-30
-a,b=map(int, input('숫자 두 개를 입력하세요: ').split(,))
-SyntaxError: invalid syntax
+# 30
+
 a,b=map(int, input('숫자 두 개를 입력하세요: ').split(','))
-숫자 두 개를 입력하세요: 10,20
+# 숫자 두 개를 입력하세요: 10,20
 print(a+b)
-30
+# 30
 
 # 연습문제
 a,b,c=map(int, input().split())
--10 20 30
+# -10 20 30
 print(a+b+c)
-40
+# 40
 
-# 심사문제
+# 심사문제 1
 a = 50; b = 100; c = None
 print(a); print(b); print(c)
-50
-100
-None
+# 50
+# 100
+# None
+
+# 심사문제 2
 kr,en,ma,sc=map(int, input().split())
-32
-Traceback (most recent call last):
-  File "<pyshell#66>", line 1, in <module>
-    kr,en,ma,sc=map(int, input().split())
-ValueError: not enough values to unpack (expected 4, got 1)
-kr,en,ma,sc=map(int, input().split())
-32 53 22 95
 print((kr+en+ma+sc)//4)
-50
+# 32 53 22 95
+# 50
