b95740d5b2a0f0cb967e9f8d04e1868bf4ebe575
SYJ <77631167+SYJ0321@users.noreply.github.com>
2022-01-19 19:23:36 +0900

    Update unit19.py

--- 코딩도장/Unit 16-21: 반복문/unit19.py
+++ 코딩도장/Unit 16-21: 반복문/unit19.py
@@ -1,36 +1,30 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license()" for more information.
 
-for i in range(5):
-    for j in range(5):
-        print('j:', j, sep='', end=' ')
-
-        
-j:0 j:1 j:2 j:3 j:4 j:0 j:1 j:2 j:3 j:4 j:0 j:1 j:2 j:3 j:4 j:0 j:1 j:2 j:3 j:4 j:0 j:1 j:2 j:3 j:4 
 for i in range(5):
     for j in range(5):
         print('j:', j, sep='', end=' ')
     print('i:', i, '\\n', sep='')
 
     
-j:0 j:1 j:2 j:3 j:4 i:0\n
-j:0 j:1 j:2 j:3 j:4 i:1\n
-j:0 j:1 j:2 j:3 j:4 i:2\n
-j:0 j:1 j:2 j:3 j:4 i:3\n
-j:0 j:1 j:2 j:3 j:4 i:4\n
+# j:0 j:1 j:2 j:3 j:4 i:0\n
+# j:0 j:1 j:2 j:3 j:4 i:1\n
+# j:0 j:1 j:2 j:3 j:4 i:2\n
+# j:0 j:1 j:2 j:3 j:4 i:3\n
+# j:0 j:1 j:2 j:3 j:4 i:4\n
 
-# 5x5 별 출력하기
+# 사각형으로 별 출력하기
 for i in range(5):
     for j in range(5):
         print('*', end='')
     print()
 
     
-*****
-*****
-*****
-*****
-*****
+# *****
+# *****
+# *****
+# *****
+# *****
 
 for i in range(3):
     for j in range(7):
@@ -38,9 +32,9 @@ for i in range(3):
     print()
 
     
-*******
-*******
-*******
+# *******
+# *******
+# *******
 
 # 계단식으로 별 출력하기
 for i in range(5):
@@ -50,11 +44,11 @@ for i in range(5):
     print()
 
     
-*
-**
-***
-****
-*****
+# *
+# **
+# ***
+# ****
+# *****
 
 # 대각선으로 별 출력하기
 for i in range(5):
@@ -66,41 +60,13 @@ for i in range(5):
     print()
 
     
-*    
- *   
-  *  
-   * 
-    *
+# *    
+#  *   
+#   *  
+#    * 
+#     *
 
 # 연습문제
-for i in range(5):
-    for j in range(5):
-        if j<=5-i:
-            print('*', end='')
-        else:
-            print(' ', end='')
-    print()
-
-    
-*****
-*****
-**** 
-***  
-**   
-for i in range(5):
-    for j in range(5):
-        if j<=i:
-            print(' ', end='')
-        else:
-            print('*', end='')
-    print()
-
-    
- ****
-  ***
-   **
-    *
-     
 for i in range(5):
     for j in range(5):
         if j<i:
@@ -110,27 +76,14 @@ for i in range(5):
     print()
 
     
-*****
- ****
-  ***
-   **
-    *
+# *****
+#  ****
+#   ***
+#    **
+#     *
 
 # 심사문제
 height = int(input())
-3
-for i in range(height):
-    for j in range(2*height+1):
-        if j<i or j>i:
-            print('*', end='')
-        else:
-            print(' ', end='')
-    print()
-
-    
- ******
-* *****
-** ****
 for i in range(height):
     for j in range(2*height+1):
         if j<height-i-1:
@@ -142,6 +95,7 @@ for i in range(height):
     print()
 
     
-  *    
- ***   
-*****  
+# 3
+#   *    
+#  ***   
+# *****  
